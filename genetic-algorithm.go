6b7f81fdd7e0d0c08de8725f9dc5caa4f1b2d257
Jessie Wilkins <jessiekwilkins.yahoo.com@gmail.com>
2024-04-19 21:45:34 -0500

    Fixed issue, refactored, and added calculation for top two fitness scores

--- genetic-algorithm.go
+++ genetic-algorithm.go
@@ -9,7 +9,10 @@ var prompt_array1 = []string{"fake", "real", "possible", "improbable", "fantasti
 
 var prompt_array2 = []string{"banana", "apple", "eggplant", "cranberry", "orange"}
 
-func fit(target string, prompt_index1 []int, prompt_index2 []int, fit_score []int) ([]int, []int, []int) {
+var top_fit_score_values = []int{0, 0}
+var top_fit_score_locations = []int{0, 0}
+
+func fit(target string, prompt_index1 []int, prompt_index2 []int, fit_score []int) ([]int, []int, []int, bool) {
 	var new_prompt_index1 = []int{0, 0, 0, 0, 0}
 	var new_prompt_index2 = []int{0, 0, 0, 0, 0}
 	var temp_fit_score = []int{0, 0, 0, 0, 0}
@@ -22,28 +25,46 @@ func fit(target string, prompt_index1 []int, prompt_index2 []int, fit_score []in
 			var sep_actual = strings.Split(actual, " ")
 
 			for i, s := range sep_actual {
-				if sep_target[i] == s {
-					temp_fit_score[i]++
-				}
-				if temp_fit_score[prompt_i] < fit_score[prompt_i] {
-					new_prompt_index1[prompt_i] = rand.Intn(len(prompt_array1))
-					new_prompt_index2[prompt_i] = rand.Intn(len(prompt_array2))
-				} else {
-					var bool_array = [2]bool{true, false}
-					fit_score[prompt_i] = temp_fit_score[prompt_i]
-					if bool_array[rand.Intn(2)] {
-						new_prompt_index1[prompt_i] = rand.Intn(len(prompt_array1))
-						new_prompt_index2[prompt_i] = prompt_index2[prompt_i]
-					} else {
-						new_prompt_index1[prompt_i] = prompt_index1[prompt_i]
-						new_prompt_index2[prompt_i] = rand.Intn(len(prompt_array2))
-					}
-				}
+				calculateFitness(sep_target, i, s, temp_fit_score, prompt_i)
+				mutate(temp_fit_score, prompt_i, fit_score, new_prompt_index1, new_prompt_index2, prompt_index2, prompt_index1)
 			}
 
 		} else {
-			return prompt_index1, prompt_index2, fit_score
+			return prompt_index1, prompt_index2, fit_score, true
 		}
 	}
-	return new_prompt_index1, new_prompt_index2, fit_score
+	return new_prompt_index1, new_prompt_index2, fit_score, false
+}
+
+func mutate(temp_fit_score []int, prompt_i int, fit_score []int, new_prompt_index1 []int, new_prompt_index2 []int, prompt_index2 []int, prompt_index1 []int) {
+	if temp_fit_score[prompt_i] < fit_score[prompt_i] {
+		new_prompt_index1[prompt_i] = rand.Intn(len(prompt_array1))
+		new_prompt_index2[prompt_i] = rand.Intn(len(prompt_array2))
+	} else {
+		var bool_array = [2]bool{true, false}
+		fit_score[prompt_i] = temp_fit_score[prompt_i]
+		if bool_array[rand.Intn(2)] {
+			new_prompt_index1[prompt_i] = rand.Intn(len(prompt_array1))
+			new_prompt_index2[prompt_i] = prompt_index2[prompt_i]
+		} else {
+			new_prompt_index1[prompt_i] = prompt_index1[prompt_i]
+			new_prompt_index2[prompt_i] = rand.Intn(len(prompt_array2))
+		}
+	}
+}
+
+func calculateFitness(sep_target []string, i int, s string, temp_fit_score []int, prompt_i int) {
+	if sep_target[i] == s {
+		temp_fit_score[prompt_i]++
+	}
+	if temp_fit_score[prompt_i] > top_fit_score_values[0] {
+		top_fit_score_values[1] = top_fit_score_values[0]
+		top_fit_score_locations[1] = top_fit_score_locations[0]
+		top_fit_score_values[0] = temp_fit_score[prompt_i]
+		top_fit_score_locations[0] = prompt_i
+	} else if temp_fit_score[prompt_i] > top_fit_score_values[1] {
+		top_fit_score_values[1] = temp_fit_score[prompt_i]
+		top_fit_score_locations[0] = prompt_i
+	}
+
 }
--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 
-	var fit_score = 0
+	var fit_score = []int{0, 0, 0, 0, 0}
 
-	var prompt_index1 = []int{0, 0, 0, 0, 0}
+	var prompt_index1 = []int{rand.Intn(5), rand.Intn(5), rand.Intn(5), rand.Intn(5), rand.Intn(5)}
 
-	var prompt_index2 = []int{0, 0, 0, 0, 0}
+	var prompt_index2 = []int{rand.Intn(5), rand.Intn(5), rand.Intn(5), rand.Intn(5), rand.Intn(5)}
 
-	for i := 0; i < 10; i++ {
-		prompt_index1, prompt_index2, fit_score = fit("real apple", prompt_index1[:], prompt_index2[:], fit_score[:])
-		for i := 0; i < 5; i++ {
+	var fit_achieved = false
 
-			fmt.Printf("Intermdiate response:  %v %v\n", prompt_array1[prompt_index1[i]], prompt_array2[prompt_index2[i]])
+	var num_of_rounds = 0
 
-		}
+	for !fit_achieved {
+		prompt_index1, prompt_index2, fit_score, fit_achieved = fit("real apple", prompt_index1, prompt_index2, fit_score)
+		num_of_rounds++
+		// for i := 0; i < 5; i++ {
+
+		// 	fmt.Printf("Intermdiate response:  %v %v\n", prompt_array1[prompt_index1[i]], prompt_array2[prompt_index2[i]])
+
+		// }
 	}
-
+	fmt.Printf("Num of Rounds: %v\n", num_of_rounds)
 	for i := 0; i < 5; i++ {
 		fmt.Printf("Final response: %v %v\n", prompt_array1[prompt_index1[i]], prompt_array2[prompt_index2[i]])
 	}
